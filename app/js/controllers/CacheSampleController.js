d19e5e098b8246323fd968cc0d5cc65e855d85aa
kevguy <kevlai22@uw.edu>
2016-10-05 16:51:47 +0800

    feat: add cacheFactory

--- app/js/controllers/CacheSampleController.js
+++ app/js/controllers/CacheSampleController.js
@@ -1,5 +1,16 @@
 'uses strict'
 
-eventsApp.controller('CacheSampleController', function CacheSampleController($scope){
+eventsApp.controller('CacheSampleController', function CacheSampleController($scope, myCache){
+    $scope.addToCache = function(key, value){
+        myCache.ut(key, value);
+    };
+
+    $scope.readFromCache = function(key) {
+        return myCache.get(key);
+    };
+
+    $scope.getCacheStats = function() {
+        return myCache.info();
+    };
 
 });
