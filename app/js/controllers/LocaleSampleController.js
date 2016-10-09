4c07256ac39bb89c266f38901621ccd3157971f7
kevguy <kevlai22@uw.edu>
2016-10-09 14:14:36 +0800

    feat: add custom exceptionHandler

--- app/js/controllers/LocaleSampleController.js
+++ app/js/controllers/LocaleSampleController.js
@@ -4,5 +4,8 @@ eventsApp.controller('LocaleSampleController',
     function LocaleSampleController($scope, $locale){
         $scope.myDate = Date.now();
         $scope.myFormat = $locale.DATETIME_FORMATS.fullDate;
+
+        // for testing exceptionHandler
+        throw {message: 'error message'};
     }
 );
