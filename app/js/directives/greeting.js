c006d706af4e7e3f411dfb4da7033da9952edee8
kevguy <kevlai22@uw.edu>
2016-11-21 00:03:23 +0800

    feat: add sharing directives

--- app/js/directives/greeting.js
+++ app/js/directives/greeting.js
@@ -1,14 +1,40 @@
 'use strict';
 
-eventsApp.directive('greeting', function(){
-    return {
-        restrict: 'E',
-        replace: true,
-        template: "<button class='btn' ng-Click='sayHello()'>Say Hello</button>",
-        controller: function($scope){
-            $scope.sayHello = function(){
-                alert("Hello");
-            };
+eventsApp
+    .directive('greeting', function(){
+        return {
+            restrict: 'E',
+            replace: true,
+            template: "<button class='btn' ng-Click='sayHello()'>Say Hello</button>",
+            controller: 'GreetingController'
+        };
+    })
+    .directive('finnish', function(){
+        return {
+            restrict: 'A',
+            require: 'greeting',
+            link: function(scope, element, attrs, controller){
+                controller.addGreeting('hei');
+            }
+
         }
-    };
+    })
+    .directive('hindi', function(){
+        return {
+            restrict: 'A',
+            require: 'greeting',
+            link : function(scope, element, attrs, controller){
+                controller.addGreeting('namaste');
+            }
+        }
+    });
+
+eventsApp.controller('GreetingController', function($scope){
+    var greetings = ['hello'];
+    $scope.sayHello = function(){
+        alert(greetings.join());
+    }
+    this.addGreeting = function(greeting){
+        greetings.push(greeting);
+    }
 });
